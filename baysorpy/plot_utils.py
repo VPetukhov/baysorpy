4adae4270abcc57f333cad3f3317a6dd075626ea
viktor_petukhov <viktor.s.petuhov@ya.ru>
2023-11-03 04:39:47 -0400

    Improved plotting: - Allow polygon drawing - Scaling point size by z - Allow colormaps for annotation

--- baysorpy/plot_utils.py
+++ baysorpy/plot_utils.py
@@ -1,13 +1,17 @@
 from pandas import DataFrame
+import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
 import numpy as np
-from typing import List, Union
+from typing import List, Union, Optional
 
 def plot_molecules(
-        df: DataFrame, color: Union[List[str], str] = None, genes: List[str] = None, gene_names: List[str] = None,
-        annotation: Union[List[str], str] = None, noise_annotation: str = None, noise_color: str = 'black',
+        df: DataFrame, polygons: Optional[List[List[float]]] = None, color: Optional[Union[List[str], str]] = None,
+        genes: Optional[List[str]] = None, gene_names: Optional[List[str]] = None, scale_z: bool = False,
+        annotation: Optional[Union[List[str], str]] = None, noise_annotation: Optional[str] = None,
+        noise_color: str = 'black', annotation_cmap: str = "tab20",
         s=1, alpha=0.1, figsize=(10, 10), ax=None, show_ticks: bool = False,
-        legend_loc: str = 'best', legend_markerscale: float = 2.0, **kwargs
+        legend_loc: str = 'best', legend_markerscale: float = 2.0, legend_title: Optional[str] = None, **kwargs
     ):
 
     if isinstance(color, str):
@@ -27,6 +31,9 @@ def plot_molecules(
         plt.figure(figsize=figsize)
         ax = plt.gca()
 
+    if scale_z:
+        s = df.z.values * s
+
     if annotation is None:
         if color is None:
             ax.scatter(df.x, df.y, s=s, alpha=alpha, **kwargs)
@@ -35,14 +42,21 @@ def plot_molecules(
     else:
         ann_levels = np.unique(annotation)
         ann_levels = [noise_annotation] + sorted(np.setdiff1d(ann_levels, noise_annotation))
-        for l in ann_levels:
+
+        cmap = matplotlib.colormaps[annotation_cmap]
+        cnorm = plt.Normalize(0, len(ann_levels))
+
+        for i,l in enumerate(ann_levels):
             mask = (annotation == l)
             cs = s[mask] if isinstance(s, np.ndarray) else s
             if (noise_annotation is not None) and (l == noise_annotation):
                 plt.scatter(df.x.values[mask], df.y.values[mask], s=cs / 4, alpha=alpha / 2, label=l, color=noise_color)
             else:
-                plt.scatter(df.x.values[mask], df.y.values[mask], s=cs, alpha=alpha, label=l)
-        plt.legend(loc=legend_loc, markerscale=legend_markerscale)
+                plt.scatter(df.x.values[mask], df.y.values[mask], s=cs, alpha=alpha, label=l, color=cmap(cnorm(i)))
+        plt.legend(loc=legend_loc, markerscale=legend_markerscale, title=legend_title)
+
+    if polygons is not None:
+        ax.add_collection(LineCollection(polygons, color="black"))
 
     ax.set_xlim(df.x.min(), df.x.max()); ax.set_ylim(df.y.min(), df.y.max());
     if not show_ticks:
